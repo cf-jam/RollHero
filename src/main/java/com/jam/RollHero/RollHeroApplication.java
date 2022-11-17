dd46196b09b7280a44d92aad3344eb3d6c333bc6
Mehtab Riar <mehtabriar228@hotmail.com>
2022-11-17 13:29:19 -0800

    test

---
+++ src/main/java/com/jam/RollHero/RollHeroApplication.java
@@ -0,0 +1,13 @@
+package com.jam.RollHero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollHeroApplication.class, args);
+	}
+
+}
